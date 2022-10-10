247a1b176adbc741b708005ca9b891d4dba69ede
Bryan Parada <bryan.parada.cea@gmail.com>
2022-10-10 20:55:12 -0300

    V85 peticion HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey: string = 'MfPJyYXAqz9iCZfIZldD3NEAtndCBaHg';
   private _historial: string [] = [ ];
 
   get historial() {
         return [...this._historial];
   }
+
+  constructor( private http: HttpClient){}
 
   buscarGifs( query: string = '' ){
 
@@ -20,8 +24,15 @@ export class GifsService {
       this._historial.unshift( query ); //insertar al inicio
       this._historial = this._historial.splice(0,10); //maximo 10
     }
+
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key='+this.apiKey+'&q=overwatch&limit=10')
+      .subscribe( (resp:any) => {
+        console.log(resp.data);
+        
+      });
  
-    console.log(this._historial);
+    // console.log(this._historial); 
+
     
 
   }
